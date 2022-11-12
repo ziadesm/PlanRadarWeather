4751f7c0959de0ffcf15166b289d66180e606730
Ziad <zi.eus@hotmail.com>
2022-11-12 20:54:50 +0200

    Update WeatherRepositoryImpTest.kt

--- app/src/androidTest/java/com/planradar/weather/data/WeatherRepositoryImpTest.kt
+++ app/src/androidTest/java/com/planradar/weather/data/WeatherRepositoryImpTest.kt
@@ -1,11 +1,5 @@
 package com.planradar.weather.data
-
-import android.content.Context
 import android.util.Log
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.test.platform.app.InstrumentationRegistry
-import com.planradar.weather.data.api.ApiConstants
 import com.planradar.weather.data.api.WeatherApi
 import com.planradar.weather.data.api.utils.FakeServer
 import com.planradar.weather.data.cache.WeatherDatabase
@@ -13,34 +7,16 @@ import com.planradar.weather.data.cache.daos.WeatherDao
 import com.planradar.weather.domain.repository.WeatherRepository
 import com.planradar.weather.domain.repository.WeatherRepositoryImpl
 import com.planradar.weather.network.NetworkResponse
-import com.planradar.weather.utils.GeneralConstants
-import dagger.Binds
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
-import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
-
 import org.junit.Test
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.Calendar
-import java.util.concurrent.TimeUnit
+import java.util.*
 import javax.inject.Inject
-import javax.inject.Singleton
-
 
 @HiltAndroidTest
 class WeatherRepositoryImpTest {
@@ -51,7 +27,7 @@ class WeatherRepositoryImpTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
-    lateinit var cache: WeatherDao
+    private lateinit var cache: WeatherDao
 
     @Inject
     lateinit var database: WeatherDatabase
@@ -59,16 +35,14 @@ class WeatherRepositoryImpTest {
     @Inject
     lateinit var api: WeatherApi
 
-    @Module
+    /*@Module
     @InstallIn(ViewModelComponent::class)
-    object ViewModelModule {
-        @Provides
-        fun provideWeather(cache: WeatherDao, api: WeatherApi): WeatherRepository {
-            return FakeRepository()
-        }
-    }
+    abstract class ViewModelModule {
+        @Binds
+        abstract fun provideWeather(api: WeatherApi, cache: WeatherDao): WeatherRepository
+    }*/
 
-    @Module
+    /*@Module
     @InstallIn(SingletonComponent::class)
     object TestCacheModule {
         @Singleton
@@ -110,16 +84,13 @@ class WeatherRepositoryImpTest {
                 .create(WeatherApi::class.java)
         }
 
-    }
+    }*/
 
     @Before
     fun setup() {
         fakeServer.start()
-
         hiltRule.inject()
-
         cache = database.weatherDao()
-
         repository = WeatherRepositoryImpl(
             cache,
             api
